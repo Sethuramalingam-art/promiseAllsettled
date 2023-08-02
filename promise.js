13f9f78fd00ab19d7be922f48f8b5795ceeca83d
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-08-02 10:12:29 +0530

    Update promise.js

--- promise.js
+++ promise.js
@@ -22,7 +22,19 @@ node:internal/process/promises:288
 [UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "give1".] {
   code: 'ERR_UNHANDLED_REJECTION'
 }
+-----------------------------------------------------------------------
+var p1 = new Promise((resolve, reject) => {
+  reject("give");
+});
 
+var p2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("give1");
+  }, 5000);
+});
+
+// it wont wait for all promise to be setttled if reject is there it should reject immediately
+--------------------------------------------------------------------------
 // promise.allSettled => either resolve or reject not an matter it should wait for all promise to be settled and return the status
 Promise.allSettled([p1, p2]).then((res) => {
   console.log(res);
